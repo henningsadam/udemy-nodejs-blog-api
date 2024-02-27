c239cd792078c2275c9d5a2f4010cfa3aa64f5e0
Adam Hennings <hennings.adam@gmail.com>
2024-02-27 11:07:50 -0700

    added endpoint to delete posts.

--- controllers/feed.js
+++ controllers/feed.js
@@ -112,7 +112,7 @@ exports.updatePost = (req, res, next) => {
         throw error;
       }
       if (imageUrl !== post.imageUrl) {
-        clearImage(post.imageUrl)
+        clearImage(post.imageUrl);
       }
       post.title = title;
       post.imageUrl = imageUrl;
@@ -121,6 +121,32 @@ exports.updatePost = (req, res, next) => {
     })
     .then((result) => {
       res.status(200).json({ message: 'Post updated', post: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      // check logged in user
+      if (!post) {
+        const error = new Error('No post found!');
+        error.status = 404;
+        throw error;
+      }
+
+      clearImage(post.imageUrl);
+      return Post.findByIdAndDelete(postId);
+    })
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({ message: 'Deleted post.' });
     })
     .catch((err) => {
       if (!err.statusCode) {
