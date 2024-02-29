85b3f84171f057f7e88cb2fb1d865638da63c7e2
Adam Hennings <hennings.adam@gmail.com>
2024-02-29 16:28:02 -0700

    converted everything to graphql

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,28 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.get('Authorization').split(' ')[1];
+  const authHeader = req.get('Authorization');
+  if (!authHeader) {
+    req.isAuth = false;
+    return next();
+  }
+  const token = authHeader.split(' ')[1];
+
   let decodedToken;
   try {
     decodedToken = jwt.verify(token, 'somesupersecret');
   } catch (error) {
     error.statusCode = 500;
-    throw error;
+    req.isAuth = false;
+    return next();
   }
   if (!decodedToken) {
     const error = new Error('Not authenticated');
-    error.statusCode = 401;
-    throw error;
+    req.isAuth = false;
+    return next();
   }
 
   req.userId = decodedToken.userId;
-  next();
+  req.isAuth = true;
+  return next();
 };
